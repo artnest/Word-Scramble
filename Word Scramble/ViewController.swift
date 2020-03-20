3a43e58fb8e829e6a7100516edd21c9aa85a5c0f
artnest <artnest789@gmail.com>
2020-03-20 14:08:42 +0300

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Artyom Nesterenko on 20/3/20.
+//  Copyright © 2020 artnest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
