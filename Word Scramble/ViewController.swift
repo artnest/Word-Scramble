7ef5f00e9524597f2ec1f83d081ba524e2fb5c73
artnest <artnest789@gmail.com>
2020-03-22 00:20:45 +0300

    Add loading of all words Add setting the title to random word

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    private var allWords = [String]()
+    private var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
         
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    private func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
 }
