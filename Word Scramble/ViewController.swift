d5eb4bd0fab46e78029302c7746f3ba31dd145e9
artnest <artnest789@gmail.com>
2020-06-04 10:27:05 +0300

    Implement checking methods

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -74,14 +74,27 @@ class ViewController: UITableViewController {
     }
     
     private func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
         return true
     }
     
     private func isOriginal(word: String) -> Bool {
-        return true
+        return !usedWords.contains(word)
     }
     
     private func isReal(word: String) -> Bool {
-        return true
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
     }
 }
